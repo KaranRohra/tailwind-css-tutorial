9ba42534e62cf1a730c1c131ae2219df97841e69
Karan Rohra <64662748+KaranRohra@users.noreply.github.com>
2024-09-18 06:55:27 +0000

    Install tailwind css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import './styles/App.css';
 
 function App() {
   return (
-    <div>Hello</div>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div className="shrink-0">
+        <img className="size-12" src="/img/logo.svg" alt="ChitChat Logo" />
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-slate-500">You have a new message!</p>
+      </div>
+    </div>
   );
 }
 
